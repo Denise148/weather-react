bedc87489b2298bc600a1a78542d2f4f9af050e2
Denise148 <167827528+Denise148@users.noreply.github.com>
2024-07-31 20:42:48 -0600

    imported weather app

---
+++ src/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by{" "}
+      <a href="https://github.com/Denise148" target="_blank" rel="noreferrer">
+        Denise Garcia{" "}
+      </a>
+      and is{" "}
+      <a
+        href="https://github.com/Denise148/SheCodes-Project-Weather-App"
+        target="_blank"
+        rel="noreferrer">
+        on GitHub{" "}
+      </a>
+      and{" "}
+      <a
+        href="https://app.netlify.com/teams/denise148/overview"
+        target="_blank"
+        rel="noreferrer">
+        hosted on Netlify
+      </a>
+    </p>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°F`
-    );
-  }
-  let apiKey = "9c2510687917a4dd804679999e5d1803";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
-    ${props.city}&appid=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState("false");
+  let [weather, setWeather] = useState({});
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.main.temp,
+      wind: response.wind.speed,
+      humidity: response.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`,
+      description: response.weather[0].description,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "9c2510687917a4dd804679999e5d1803";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
+    ${city}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then((response) => {
+      displayWeather(response.data);
+    });
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°F</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}mp/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
